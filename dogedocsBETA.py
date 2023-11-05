eac9e604e9a1b79e358bf5089fb471f5057ba394
malachi196 <148344389+malachi196@users.noreply.github.com>
2023-11-05 17:56:53 -0600

    THIS IS STILL UNDER HEAVY CONTSTRUCTION

---
+++ dogedocsBETA.py
@@ -0,0 +1,13 @@
+from tkinter import *
+import subsearch as ss
+#=====tkinter var/funtions=====
+root = Tk()
+#=====subsearch var/funtions=====
+
+#=====run the program=====
+ss.searchbar()
+
+ss.createBTN(title="hello")
+
+
+root.mainloop()
